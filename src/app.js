8ab6ce55458d283b11c26ba342662279a5b7fc62
dreadpool <itsdreadpool@dreadpool.com>
2021-07-26 09:51:23 +0530

    code cleanup

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ require("./db/mongoose");
 
 const userRouter = require("./routers/user");
 
-const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
@@ -19,34 +18,6 @@ app.use(express.json());
 
 app.use(userRouter);
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Skynox Task API",
-      description:
-        "An API built with Express and MongoDB. This API provides a secure user registration and login system which can persist the login state in the backend itself.",
-    },
-  },
-  components: {
-    securitySchemes: {
-      jwt: {
-        type: "http",
-        scheme: "bearer",
-        in: "header",
-        bearerFormat: "JWT",
-      },
-    },
-  },
-  security: [
-    {
-      jwt: [],
-    },
-  ],
-  apis: ["./src/routers/user.js"],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
